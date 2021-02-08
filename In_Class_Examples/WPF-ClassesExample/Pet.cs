eaf0bbdf2a1d463f7f62f032dc49dbbd35e3844c
Ackerman, Adam P <adam@ou.edu>
2021-02-08 11:54:28 -0600

    Added another example using classes

---
+++ In_Class_Examples/WPF-ClassesExample/Pet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_ClassesExample
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string PicURL { get; set; }
+
+
+        public Pet()
+        {
+            Name = string.Empty;
+            Breed = string.Empty;
+            PicURL= string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Breed})";
+        }
+
+    }
+}
