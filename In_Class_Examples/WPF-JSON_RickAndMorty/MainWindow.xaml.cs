603477999ef8ff97954296c4f8494c3a2cc11601
Ackerman, Adam P <adam@ou.edu>
2021-02-22 12:20:01 -0600

    Did a JSON example in WPF

---
+++ In_Class_Examples/WPF-JSON_RickAndMorty/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_JSON_RickAndMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var client = new HttpClient())
+            {
+
+                //https://rickandmortyapi.com/api/character
+
+                var json = client.GetStringAsync("https://rickandmortyapi.com/api/character").Result;
+
+                RickAndMortyAPI api = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+
+                foreach (var character in api.results)
+                {
+                    lstCharacters.Items.Add(character); 
+                }
+
+            }
+
+        }
+    }
+}
---
+++ In_Class_Examples/WPF-JSON_RickAndMorty/RickAndMortyAPI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_JSON_RickAndMorty
+{
+    public class RickAndMortyAPI
+    {
+        public Info  info {get;set; }
+
+        public List<Character> results { get; set; }
+    }
+
+    public class Character
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public string status { get; set; }
+
+        public string image { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name} ({id})";
+        }
+
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+
+        public string next { get; set; }
+
+    }
+
+}
