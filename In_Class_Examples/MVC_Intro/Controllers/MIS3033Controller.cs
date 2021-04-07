bd174790ed74b6113a3d7a6b9bc328ef1525a810
Ackerman, Adam P <adam@ou.edu>
2021-04-07 12:24:12 -0500

    example for MVC

---
+++ In_Class_Examples/MVC_Intro/Controllers/MIS3033Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Intro.Controllers
+{
+    public class MIS3033Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            Student s = new Student()
+            {
+                FirstName = "Micah",
+                LastName = "Tison"
+            };
+            return View(s);
+        }
+    }
+}
